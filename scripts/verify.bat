6a4cea5ee9e2c66d5faa2714fc30798ee13cd76e
Jack Gallagher <jackgllghr@gmail.com>
2023-07-23 03:51:55 +0100

    Clean up manifest

--- scripts/verify.bat
+++ scripts/verify.bat
@@ -1,2 +1,2 @@
-..\..\AppData\Local\Playnite\Toolbox.exe verify addon extension.yaml
+..\..\AppData\Local\Playnite\Toolbox.exe verify addon manifest.yaml
 ..\..\AppData\Local\Playnite\Toolbox.exe verify installer installer.yaml
