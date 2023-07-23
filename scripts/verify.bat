7141cdcf8426f8c329ea1f5e7b9e562d4d6ebff0
Jack Gallagher <jackgllghr@gmail.com>
2023-07-23 03:41:24 +0100

    Fix for manifest file

--- scripts/verify.bat
+++ scripts/verify.bat
@@ -1,2 +1,2 @@
-..\..\AppData\Local\Playnite\Toolbox.exe verify installer installer.yaml
 ..\..\AppData\Local\Playnite\Toolbox.exe verify addon extension.yaml
+..\..\AppData\Local\Playnite\Toolbox.exe verify installer installer.yaml
